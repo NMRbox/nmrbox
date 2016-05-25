a50dfbf85c7c7d5bddb22bda7be9460df7e0712d
djones <djones@33504fd2-e2af-4168-9044-610c50ee6ce2>
2016-05-25 23:53:45 +0000

    Alphabetized packages on software registry page.
    Fixes https://nmrbox-devel.cam.uchc.edu/bugzilla/show_bug.cgi?id=31
    
    git-svn-id: https://devel.nmrbox.org/svn/nmrbox/trunk/web@898 33504fd2-e2af-4168-9044-610c50ee6ce2

--- website/app/Http/Controllers/RegistryController.php
+++ website/app/Http/Controllers/RegistryController.php
@@ -15,14 +15,13 @@ class RegistryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $all_software = Software::All();
+    public function index() {
+        $all_software = Software::All()->sortBy('name');
         return View::make("registry.index", compact('all_software'));
     }
 
     /**
-     * Display the inde of a particular software package.
+     * Display the index of a particular software package.
      *
      * @return \Illuminate\Http\Response
      */
@@ -37,67 +36,5 @@ class RegistryController extends Controller
         $vm_versions = VM::all();
 
         return View::make("registry.software", compact('software', 'all_files', 'vm_versions', 'attached_citations'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('admin.vms.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $vm = new VM($request->all());
-        $vm->name = $vm->major . "." . $vm->minor . "." . $vm->variant;
-        $vm->save();
-        return redirect('admin/vm');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(VM $vm)
-    {
-        return view('admin.vms.edit', compact('vm'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(VMRequest $request, VM $vm)
-    {
-        $vm->update($request->all());
-        $vm->name = $vm->major . "." . $vm->minor . "." . $vm->variant;
-        $vm->save(); // yes, this means two writes to db. Can be refactored.
-        return redirect('admin/vm');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(VM $vm)
-    {
-        $vm->delete();
-        return redirect('admin/vm');
     }
 }
