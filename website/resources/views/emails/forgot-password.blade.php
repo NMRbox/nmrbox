2bc8c2e6efc19a9a679a857193fa8dcd0f03df46
mchowdhury <mchowdhury@33504fd2-e2af-4168-9044-610c50ee6ce2>
2017-01-09 22:32:03 +0000

    Modified forget password email template and fixed reminder table entries.
    git-svn-id: https://devel.nmrbox.org/svn/nmrbox/trunk/web@2461 33504fd2-e2af-4168-9044-610c50ee6ce2

--- website/resources/views/emails/forgot-password.blade.php
+++ website/resources/views/emails/forgot-password.blade.php
@@ -1,13 +1,12 @@
 @extends('emails/layouts/default')
 
 @section('content')
-<p>Hello {!! $user->first_name !!} {!! $user->last_name !!},</p>
+<p>Dear {!! $user->first_name !!} {!! $user->last_name !!},</p>
 
-<p>Please click on the following link to update your password:</p>
-
-<p><a href="{!! $forgotPasswordUrl !!}">Reset password</a></p>
+<p>We received a request to reset your password for NMRBox username - <b>{!! $user->nmrbox_acct !!}</b></p>
+<p>Please <a href="{!! $forgotPasswordUrl !!}">click here</a> and follow the prompts to update your password.</p>
+<p>If you did not make the request, just ignore this email or contact NMRBox support. </p>
 
 <p>Best regards,</p>
-
 <p>@lang('general.site_name') Team</p>
 @stop
