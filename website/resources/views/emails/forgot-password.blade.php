ce4c54ea128d2c4940c29ff03d46b086179a8fcd
mchowdhury <mchowdhury@33504fd2-e2af-4168-9044-610c50ee6ce2>
2017-01-10 15:52:19 +0000

    Modified forget password email content.
    git-svn-id: https://devel.nmrbox.org/svn/nmrbox/trunk/web@2463 33504fd2-e2af-4168-9044-610c50ee6ce2

--- website/resources/views/emails/forgot-password.blade.php
+++ website/resources/views/emails/forgot-password.blade.php
@@ -1,11 +1,11 @@
 @extends('emails/layouts/default')
 
 @section('content')
-<p>Dear {!! $user->first_name !!} {!! $user->last_name !!},</p>
+<p>Dear {!! $user->first_name !!},</p>
 
 <p>We received a request to reset your password for NMRBox username - <b>{!! $user->nmrbox_acct !!}</b></p>
 <p>Please <a href="{!! $forgotPasswordUrl !!}">click here</a> and follow the prompts to update your password.</p>
-<p>If you did not make the request, just ignore this email or contact NMRBox support. </p>
+<p>If you did not request a password reset, please contact us at: <a href= "mailto:support@nmrbox.org">support@nmrbox.org</a></p>
 
 <p>Best regards,</p>
 <p>@lang('general.site_name') Team</p>
