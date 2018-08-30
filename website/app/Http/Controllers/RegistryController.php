e2a25f8b820100067d29495c82418e3d9aaa6d6b
mchowdhury <mchowdhury@33504fd2-e2af-4168-9044-610c50ee6ce2>
2018-08-30 20:34:11 +0000

    resolved error on registry endpoint.
    git-svn-id: https://devel.nmrbox.org/svn/nmrbox/trunk/web@6513 33504fd2-e2af-4168-9044-610c50ee6ce2

--- website/app/Http/Controllers/RegistryController.php
+++ website/app/Http/Controllers/RegistryController.php
@@ -43,7 +43,7 @@ class RegistryController extends Controller
                 $research_id = $research->id;
                 $research_label = $research->research;
 
-                $all_software['research_problems'] = array(
+                $all_software['research_problems'][] = array(
                     'id' => $research_id,
                     'label' => $research_label,
                 );
@@ -54,9 +54,9 @@ class RegistryController extends Controller
             foreach ($all_soft_types as $k => $data) {
                 $stype = Stype::where('id', $data->stype_id)->get()->first();
                 $stype_id = $stype->id;
-                $stype_label = $stype->research;
+                $stype_label = $stype->stype;
 
-                $all_software['software_type'] = array(
+                $all_software['software_type'][] = array(
                     'id' => $stype_id,
                     'label' => $stype_label,
                 );
