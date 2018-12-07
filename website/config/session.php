ec3c271ca67bce669239c7fa2b22f510a84bdb3d
mchowdhury <mchowdhury@33504fd2-e2af-4168-9044-610c50ee6ce2>
2018-12-07 17:44:36 +0000

    Refactoring session variable.
    git-svn-id: https://devel.nmrbox.org/svn/nmrbox/trunk/web@7023 33504fd2-e2af-4168-9044-610c50ee6ce2

--- website/config/session.php
+++ website/config/session.php
@@ -109,7 +109,7 @@ return [
     |
     */
 
-    'cookie' => 'laravel_session',
+    'cookie' => 'nmrbox_apidev_session',
 
     /*
     |--------------------------------------------------------------------------
