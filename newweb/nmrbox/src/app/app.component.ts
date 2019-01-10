f95102ffb81a9c7e003c93f991f5d3ceb558f505
jwedell <jwedell@33504fd2-e2af-4168-9044-610c50ee6ce2>
2019-01-10 21:33:03 +0000

    Simplify app component since redirect happens in authservice.
    git-svn-id: https://devel.nmrbox.org/svn/nmrbox/trunk/web@7248 33504fd2-e2af-4168-9044-610c50ee6ce2

--- newweb/nmrbox/src/app/app.component.ts
+++ newweb/nmrbox/src/app/app.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 
 /* Import the service */
 import {AuthenticationService} from './authentication/authentication.service';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +10,12 @@ import {Router} from '@angular/router';
 })
 export class AppComponent {
   constructor(
-    private router: Router,
     public authService: AuthenticationService
   ) {
   }
 
   signOut() {
     this.authService.signOut();
-    this.router.navigateByUrl('');
   }
 
 
