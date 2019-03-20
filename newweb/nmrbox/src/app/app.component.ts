1170e0be2dbfc93f50978fab0201077ca09dc27a
jwedell <jwedell@33504fd2-e2af-4168-9044-610c50ee6ce2>
2019-03-20 12:49:50 +0000

    Improvements to responsiveService to make it work based on screen size rather than device.
    git-svn-id: https://devel.nmrbox.org/svn/nmrbox/trunk/web@7724 33504fd2-e2af-4168-9044-610c50ee6ce2

--- newweb/nmrbox/src/app/app.component.ts
+++ newweb/nmrbox/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 
 /* Import the service */
 import {AuthenticationService} from './authentication/authentication.service';
+import {ResponsiveService} from './responsive.service';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,11 @@ import {AuthenticationService} from './authentication/authentication.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  // The "unused" responsiveService is here to ensure the small screen pop-up always occurs
   constructor(
-    public authService: AuthenticationService
+    public authService: AuthenticationService,
+    private responsiveService: ResponsiveService
   ) {
   }
 
