a530944dd91a1917891345a683a4af1beaf85eb2
mchowdhury <mchowdhury@33504fd2-e2af-4168-9044-610c50ee6ce2>
2019-06-05 15:14:19 +0000

    Fix middleware session issues.
    git-svn-id: https://devel.nmrbox.org/svn/nmrbox/trunk/web@8567 33504fd2-e2af-4168-9044-610c50ee6ce2

--- NMRbox/website/app/Http/Middleware/SentinelAdmin.php
+++ NMRbox/website/app/Http/Middleware/SentinelAdmin.php
@@ -8,26 +8,28 @@ use Redirect;
 use Session;
 use App\NmrboxSession;
 
-class SentinelAdmin
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        /* if request coming with username from Angular frontend*/
-        if($request->username)
-            Session::put('username', $request->username);
+class SentinelAdmin {
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param \Closure                 $next
+	 *
+	 * @return mixed
+	 */
+	public function handle( $request, Closure $next ) {
+		/* if request coming with username from Angular frontend*/
+		if ( $request->username ) {
+			Session::put( 'username', $request->username );
+		}
 
-        if(empty(Session::get('person')) &&  empty(Session::get('token')) && empty(Session::get('user_is_admin'))) {
-            Session::flush();
-            return Redirect::route('login');
-        }
+		if ( Session::has( 'user_is_admin' ) && Session::get( 'user_is_admin' ) === false ) {
+			// Destroying the session
+			Session::flush();
 
-        return $next($request);
-    }
+			return Redirect::route( 'login' );
+		}
+
+		return $next( $request );
+	}
 }
