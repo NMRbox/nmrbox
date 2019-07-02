2c1388fac7d0c98230306b91e7953c9d4efa31ba
jwedell <jwedell@33504fd2-e2af-4168-9044-610c50ee6ce2>
2019-07-02 18:18:09 +0000

    Okay, the show/hide password is fully working as intended. Have to use text on the sign in page.
    git-svn-id: https://devel.nmrbox.org/svn/nmrbox/trunk/web@9014 33504fd2-e2af-4168-9044-610c50ee6ce2

--- NMRbox/newweb/nmrbox/src/app/app-password.directive.ts
+++ NMRbox/newweb/nmrbox/src/app/app-password.directive.ts
@@ -1,34 +1,64 @@
-import { Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appPassword]'
 })
 export class AppPasswordDirective {
   private _shown = false;
+  words = false;
 
   constructor(private el: ElementRef) {
+    if (el.nativeElement.parentNode.nodeName.toLowerCase() === 'div') {
+      this.words = true;
+    }
     this.setup();
   }
 
-  toggle(span: HTMLElement) {
+  toggle(elem: HTMLElement) {
     this._shown = !this._shown;
     if (this._shown) {
       this.el.nativeElement.setAttribute('type', 'text');
-      span.innerHTML = 'Hide password';
+      if (this.words) {
+        elem.innerHTML = 'Hide password';
+      } else {
+        elem.setAttribute('alt', 'Hide password');
+        elem.setAttribute('src', '/assets/graphics/sitewide/eye-off-outline.svg');
+        elem.title = 'Hide password';
+      }
     } else {
       this.el.nativeElement.setAttribute('type', 'password');
-      span.innerHTML = 'Show password';
+      if (this.words) {
+        elem.innerHTML = 'Show password';
+      } else {
+        elem.setAttribute('alt', 'Show password');
+        elem.setAttribute('src', '/assets/graphics/sitewide/eye.svg');
+        elem.title = 'Show password';
+      }
     }
   }
 
   setup() {
     const parent = this.el.nativeElement.parentNode;
-    const span = document.createElement('span');
-    span.innerHTML = `Show password`;
-    span.style.cursor = 'pointer';
-    span.addEventListener('click', (event) => {
-      this.toggle(span);
+    let elem;
+    if (this.words) {
+      elem = document.createElement('span');
+      elem.innerHTML = `Show password`;
+      elem.style.cursor = 'pointer';
+      elem.style.fontWeight = 'bold';
+    } else {
+      elem = document.createElement('img');
+      elem.src = '/assets/graphics/sitewide/eye.svg';
+      elem.alt = 'Show password';
+      elem.title = 'Show password';
+      elem.style.width = '24px';
+      elem.style.height = '24px';
+      elem.style.cursor = 'pointer';
+    }
+    /*
+    */
+    elem.addEventListener('click', () => {
+      this.toggle(elem);
     });
-    parent.appendChild(span);
+    parent.appendChild(elem);
   }
 }
